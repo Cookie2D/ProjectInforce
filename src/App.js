5e64ea12ce68b591c6423ad81d3188cb2c24c993
Ihor Sharov <cookie50ds@gmail.com>
2022-04-28 18:41:27 +0300

    added pagination

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import axios from 'axios'
 import {useEffect, useState} from "react";
-import './components/card/card.css'
-import Card from "./components/card/Card";
+import './components/Card/card.css'
+import Card from "./components/Card/Card";
+
+import './components/Pagination/pagination.css'
+import Pagination from "./components/Pagination/Pagination";
 
 function App() {
   const dataURL = 'https://jsonplaceholder.typicode.com'
+
   const [posts, setPosts] = useState([]);
+  const [currentPosts, setCurrentPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     axios.get(`${dataURL}/posts`)
-      .then(res => {setPosts(res.data)
-        console.log(res)})
+      .then(res => {
+        setPosts(res.data)
+        setLoading(false)
+      })
   }, [])
+
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
 
   return (
     <div className="App">
+      {currentPosts.map((card) => <Card card={card} key={card.id}/>)}
 
-
-      {posts.map((card) => <Card card={card} key={card.id} />)}
-
-
-
+      <Pagination
+        setCurrentPosts={setCurrentPosts}
+        posts={posts}
+      />
     </div>
   );
 }
---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,52 @@
+import React, {useEffect, useState} from 'react';
+import './pagination.css';
+
+const Pagination = ({setCurrentPosts, posts}) => {
+  const pages = []
+  const pageLimit = 10;
+
+  const [page, setPage] = useState(1);
+
+  for (let i = 1; i <= Math.ceil(posts.length / pageLimit); i++) {
+    pages.push(i);
+  }
+
+  useEffect(() => {
+    const lastPostId = page * pageLimit;
+    const firstPostId = lastPostId - pageLimit;
+    setCurrentPosts(posts.slice(firstPostId, lastPostId));
+  }, [page, posts])
+
+  const handlePageChange = (page) => setPage(page)
+  const incrementPage = () => {
+    if (page < pages.length) {
+      setPage(page + 1);
+    }
+  }
+  const decrementPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  }
+
+
+  return (
+    <nav className="Pagination">
+      <button onClick={() => decrementPage()} className="button">{"<"}</button>
+      <div>
+      {pages.map(num =>(
+        <button
+          onClick={() => handlePageChange(num)}
+          key={num}
+          className={page === num? "button active" : "button"}
+        >
+          {num}
+        </button>)
+      )}
+      </div>
+      <button onClick={() => incrementPage()} className="button">{">"}</button>
+    </nav>
+  );
+};
+
+export default Pagination;
